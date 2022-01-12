01ed87fe846521110a8f8dbd6524134bc46494b2
unknown <lucie@naffien.com>
2022-01-12 03:26:04 +0100

    Commentaires : en-têtes

--- Matrice.java
+++ Matrice.java
@@ -1,3 +1,16 @@
+/***********************************************************************************************************************************************
+
+                                                                Matrice
+                                                                            par NAFFIEN Lucie (janvier 2022)
+
+Cette interface appartient a un projet realise pour le module "Programmation objet multiplateformes", enseigne par M. MARIAGE, dans le cadre 
+de la 3eme annee de licence parcours Informatique des Systemes Embarques et Interactifs de l'Universite Paris 8.
+
+Lien github de l'ensemble du projet : https://github.com/lnaffien/L3_ISEI_S5-Programmation_objet_multiplateformes
+
+
+************************************************************************************************************************************************/
+
 public interface Matrice
 {
     /* 
--- MatriceDense.java
+++ MatriceDense.java
@@ -1,3 +1,16 @@
+/***********************************************************************************************************************************************
+
+                                                                MatriceDense
+                                                                            par NAFFIEN Lucie (janvier 2022)
+
+Cette classe appartient a un projet realise pour le module "Programmation objet multiplateformes", enseigne par M. MARIAGE, dans le cadre 
+de la 3eme annee de licence parcours Informatique des Systemes Embarques et Interactifs de l'Universite Paris 8.
+
+Lien github de l'ensemble du projet : https://github.com/lnaffien/L3_ISEI_S5-Programmation_objet_multiplateformes
+
+
+************************************************************************************************************************************************/
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -35,6 +48,8 @@ public class MatriceDense implements Matrice
         }
     }
 
+    /* existe
+     */
     private int existeDeja(int x, int y)
     {
         for(ArrayList<Integer> ligne : matrice)
